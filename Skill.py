aa5551efdefcbb9f30af117ecdb6519cf96a832c
makabaka <3084608158@qq.com>
2023-01-07 23:32:42 +0800

    -

--- Skill.py
+++ Skill.py
@@ -3,18 +3,16 @@ import Player
 
 
 class Skill:
-    def __init__(self, name, player: Player.player, target: Player.player, label=None, consume=None):
+    def __init__(self, name, target=None, label=None, consume=None):
         """
 
         name: 技能名称
-        player: 发动角色
         target: 目标
         consume: 消耗
         label: 技能标签(锁定技、觉醒技、限定技、主公技)
 
         """
         self.name = name
-        self.player = player
         self.target = target
         self.consume = consume
         self.label = label
@@ -22,15 +20,18 @@ class Skill:
 
 # 状态技
 class StateSkill(Skill):
-    def __init__(self, name, player, target, label=None, consume=None):
-        super(StateSkill, self).__init__(name, player, target, label, consume)
+    def __init__(self, name, target=None, label=None, consume=None):
+        super(StateSkill, self).__init__(name, target, label, consume)
 
 
-# 触发技
-class TriggerSkill(Skill):
-    def __init__(self, name, player, target, time, label=None, consume=None):
-        super(TriggerSkill, self).__init__(name, player, target, label, consume)
-        self.time = time
+class JianXiong(Skill):
+    def __init__(self):
+        self.name = '奸雄'
+        super(JianXiong).__init__(self.name)
 
-    def JianXiong(self, channel):
-        self.player.HandCards_area += channel
+    def trigger(self, channel):
+        self.target.HandCards_area.append(channel)
+        del channel
+
+
+Jianxiong = JianXiong()
